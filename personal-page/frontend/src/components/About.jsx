9e70dab31adea367eb95c961a091dd9dbb1108bc
andrew <andrew891221@gmail.com>
2024-04-12 04:12:45 +0800

    Make this page responsive

--- personal-page/frontend/src/components/About.jsx
+++ personal-page/frontend/src/components/About.jsx
@@ -1,18 +1,27 @@
-import my_image from '../assets/my-image.jpg'
+import my_image from "../assets/my-image.jpg";
 
 export default function About() {
   return (
-    <div className= 'flex flex-col basis-11/12 mx-40 mt-16'>
-        <div className='basis-1/3 flex flex-row'>
-          <img src={my_image} className='w-60 h-60 ml-20 rounded-full'/>
-          <p className='text-9xl font-serif my-auto ml-16 text-emerald-950'>Andrew Hsieh</p>
-        </div>
-        <div className='mt-6 mx-12 text-lg leading-8 text-gray-600'>
-        Currently pursuing my master's degree in deep learning at National Taiwan University, I'm deeply passionate about computer science, with special interests in deep learning and web development. 
-        Alongside my academic pursuits, I make time for regular workouts, believing in the importance of a balanced lifestyle. 
-        I also enjoy keeping small animals, which adds a touch of warmth to my tech-focused life. 
-        This blend of interests showcases my journey through the exciting world of technology while staying grounded with simple joys.
-        </div>
+    <div className="flex flex-col basis-11/12 sm:mx-40 mt-16">
+      <div className="basis-1/3 flex flex-row flex-wrap">
+        <img
+          src={my_image}
+          className="aspect-square max-w-60 max-h-60 ml-20 rounded-full"
+        />
+        <p className="text-5xl sm:text-9xl font-serif my-auto ml-16 text-emerald-950">
+          Andrew Hsieh
+        </p>
+      </div>
+      <div className="mt-6 mx-12 sm:text-lg leading-8 text-gray-600">
+        Currently pursuing my master's degree in deep learning at National
+        Taiwan University, I'm deeply passionate about computer science, with
+        special interests in deep learning and web development. Alongside my
+        academic pursuits, I make time for regular workouts, believing in the
+        importance of a balanced lifestyle. I also enjoy keeping small animals,
+        which adds a touch of warmth to my tech-focused life. This blend of
+        interests showcases my journey through the exciting world of technology
+        while staying grounded with simple joys.
+      </div>
     </div>
-  )
+  );
 }
