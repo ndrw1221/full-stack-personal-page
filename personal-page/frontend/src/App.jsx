cb8d574539537db718ad9818708d1b2663eb8ad3
andrew <andrew891221@gmail.com>
2024-04-12 06:32:25 +0800

    Avoid log out during reload

--- personal-page/frontend/src/App.jsx
+++ personal-page/frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import Header from "./components/Header.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { AuthContext } from "./contexts/AuthContext.js";
+import Header from "./components/Header.jsx";
+
+function getSavedAuthStatus() {
+  const savedAuthStatus = localStorage.getItem("isAuthenticated");
+  return savedAuthStatus !== null ? JSON.parse(savedAuthStatus) : false;
+}
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(getSavedAuthStatus());
+
+  useEffect(() => {
+    localStorage.setItem("isAuthenticated", JSON.stringify(isAuthenticated));
+  }, [isAuthenticated]);
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
